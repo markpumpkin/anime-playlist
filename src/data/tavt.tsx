5d0a677fb574438098efa5bcdaeea245c1e2fd38
markpumpkin <ngobacquang@gmail.com>
2023-07-01 21:44:30 +0700

    update data

--- src/data/tavt.tsx
+++ src/data/tavt.tsx
@@ -3,6 +3,76 @@ import { ItemData } from ".";
 export const data: ItemData[] = [
     {
         source: "youtube",
+        value: "yWB147H2WTc",
+        label: "Thần Ấn Vương Toạ | Tập 50",
+        thumbnail:
+            "https://i.pinimg.com/originals/53/7e/70/537e700ef72d96238f8d2035052bbb72.png",
+    },
+    {
+        source: "youtube",
+        value: "NMhN-bb_0Po",
+        label: "Thần Ấn Vương Toạ | Tập 51",
+        thumbnail:
+            "https://i.pinimg.com/originals/9b/7e/8c/9b7e8c01b80c757c90531b193ef6c015.png",
+    },
+    {
+        source: "youtube",
+        value: "SpqIrR2Vojg",
+        label: "Thần Ấn Vương Toạ | Tập 52",
+        thumbnail:
+            "https://i.pinimg.com/originals/49/82/f6/4982f667760568cedc4dcebc90bb7159.png",
+    },
+    {
+        source: "youtube",
+        value: "rutV8L5AZf0",
+        label: "Thần Ấn Vương Toạ | Tập 53",
+        thumbnail:
+            "https://i.pinimg.com/originals/59/ab/9e/59ab9e608c1270ba74b0087789420ee6.png",
+    },
+    {
+        source: "youtube",
+        value: "XpbDH2VsbIc",
+        label: "Thần Ấn Vương Toạ | Tập 54",
+        thumbnail:
+            "https://i.pinimg.com/originals/60/09/c3/6009c3e5c21f63c3da972733a6a7045f.png",
+    },
+    {
+        source: "youtube",
+        value: "DLsok3L0wQM",
+        label: "Thần Ấn Vương Toạ | Tập 55",
+        thumbnail:
+            "https://i.pinimg.com/originals/74/df/d2/74dfd2d0ef60136923b805067328ed0e.png",
+    },
+    {
+        source: "youtube",
+        value: "TeWwIQTSqE8",
+        label: "Thần Ấn Vương Toạ | Tập 56",
+        thumbnail:
+            "https://i.pinimg.com/originals/ad/24/1f/ad241f9a10d8bdde8c36881c8fc62d20.png",
+    },
+    {
+        source: "youtube",
+        value: "ROxKmhDz8yQ",
+        label: "Thần Ấn Vương Toạ | Tập 57",
+        thumbnail:
+            "https://i.pinimg.com/originals/98/b6/48/98b648ab501ed5d7bd7b27d11e97ea57.png",
+    },
+    {
+        source: "youtube",
+        value: "TMICuOSRXl0",
+        label: "Thần Ấn Vương Toạ | Tập 58",
+        thumbnail:
+            "https://i.pinimg.com/originals/83/86/7d/83867de2af62257b5c14d04bc42c2902.png",
+    },
+    {
+        source: "youtube",
+        value: "3P_hDASmkCY",
+        label: "Thần Ấn Vương Toạ | Tập 59",
+        thumbnail:
+            "https://i.pinimg.com/originals/7e/e4/40/7ee44074c610068328f62f52dd57e3c5.png",
+    },
+    {
+        source: "youtube",
         value: "KF7whBJo3-w",
         label: "Thần Ấn Vương Toạ | Tập 60",
         thumbnail:
